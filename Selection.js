6922df05b11edfb60be70a86d5ff47e25501e6be
Khushal <khushalvaghasiya0@gmail.com>
2024-12-10 10:30:46 +0530

    10-12-24

---
+++ Selection.js
@@ -0,0 +1,59 @@
+// 1. Enter two value find smallest.
+
+// let a , b;
+
+// if (a < b)
+// {
+//     console.log("A is Minimum");
+// }
+// else
+// {
+//     console.log("B is Minimum");
+// }
+
+
+// 2. Enter one value find its positive or negative.
+
+// let n = 65;
+
+// if (n > 0)
+// {
+//     console.log("Number is Positive");
+// }
+// else if(n == 0)
+// {
+//     console.log("Number is Zero");
+// }
+// else
+// {
+//     console.log("Number is Negative")
+// }
+
+
+// 3. Enter one value find its divisible by 5 or not.
+
+// let n = 56;
+
+// if(n % 5 == 0)
+// {
+//     console.log("Number is Divisible by 5");
+// }
+// else
+// {
+//     console.log("Number is not Divisible by 5")
+// }
+
+
+
+// Enter one value find its divisible by 5 and 3 or not.
+
+// let n = 34;
+
+// if(n % 5 == 0 && n % 3 == 0)
+// {
+//     console.log("The Number is Divisible by 5 and 3");
+// }
+// else
+// {
+
+// }
