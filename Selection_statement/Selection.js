e8bff4a2c6213240d0c361432d36878724397b95
Khushal <khushalvaghasiya0@gmail.com>
2024-12-11 10:23:41 +0530

    11-12-24

--- Selection_statement/Selection.js
+++ Selection_statement/Selection.js
@@ -11,7 +11,6 @@
 //     console.log("B is Minimum");
 // }
 
-
 // 2. Enter one value find its positive or negative.
 
 // let n = 65;
@@ -30,6 +29,8 @@
 // }
 
 
+
+
 // 3. Enter one value find its divisible by 5 or not.
 
 // let n = 56;
@@ -45,9 +46,11 @@
 
 
 
-// Enter one value find its divisible by 5 and 3 or not.
 
-// let n = 34;
+
+// 4. Enter one value find its divisible by 5 and 3 or not.
+
+// let n = 90;
 
 // if(n % 5 == 0 && n % 3 == 0)
 // {
@@ -55,5 +58,63 @@
 // }
 // else
 // {
+//     console.log("The Number is not Divisible by 5 and 3");
+// }
 
+
+
+
+
+//.5  Enter Students marks wheather pass or not.
+
+// let marks = 105;
+
+// if (marks >= 40 && marks <= 100)
+// {
+//     console.log("Student is Pass");
+// } 
+// else if(marks >= 100)
+// {
+//     console.log("Plz Enter Valid marks");
+// }
+// else
+// {
+//     console.log("Student is Fail");
+// }
+
+
+
+
+
+// 6. Enter year find its leap year or not.
+
+// let year = 2021;
+
+// if(year % 4 == 0)
+// {
+//     console.log("It is a leap year");
+// }
+// else
+// {
+//     console.log("It is not a leap year");
+// }
+
+
+
+
+// 7.Enter a, b, c find largest among them.
+
+// let a=100,b=65,c=64;
+
+// if(a > b && a > c)
+// {
+//     console.log("A is largest");
+// }
+// else if(b > a && b > c)
+// {
+//     console.log("B is largest");
+// }
+// else
+// {
+//     console.log("C is largest");
 // }
