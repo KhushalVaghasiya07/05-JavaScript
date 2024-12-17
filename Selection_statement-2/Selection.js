1408d1da79fb90ae549b83ee1cf0b2bab9a637d0
Khushal <khushalvaghasiya0@gmail.com>
2024-12-17 10:40:54 +0530

    17-12-24

---
+++ Selection_statement-2/Selection.js
@@ -0,0 +1,22 @@
+//2.C program to check whether a person is eligible for voting or not?
+
+// let age = 18;
+
+// if (age <= 0)
+// {
+//   console.log("Plz Enter valid choice");
+// }
+// else if (age < 18) 
+// {
+//   console.log("You are not Eligible for Voting");
+// }
+// else if (age >= 18) 
+// {
+//       console.log("You are Eligible for Voting");
+// }
+
+
+
+
+
+
