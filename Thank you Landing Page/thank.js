2785b831e3149d39e840d2da9c8da407eac25d42
Khushal Vaghasiya <khushalvaghasiya0@gmail.com>
2025-04-26 17:52:06 +0530

    add thank you file

---
+++ Thank you Landing Page/thank.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      document.querySelector('.fire-emoji').style.animation = 'bounce 0.8s infinite alternate';
+    }, 2000);
+  });
